87bffb0551f8a92cbae65aebc6ae4f7c6bb0e9a1
zcm <597714572@qq.com>
2017-11-13 14:45:44 +0800

    get every tab

--- MySpider.py
+++ MySpider.py
@@ -1,20 +1,28 @@
 import csv,requests,re
 from bs4 import BeautifulSoup
 
-url = 'https://www.v2ex.com/?tab=all'
-html = requests.get(url).text
+urlmode = 'https://www.v2ex.com/?tab='
+
+urlorig = 'https://www.v2ex.com/?tab=all'
+html = requests.get(urlorig).text
 soup = BeautifulSoup(html, 'html.parser')
 articles = []
-for article in soup.find_all(class_='cell item'):
-    title = article.find(class_='item_title').get_text()
-    category = article.find(class_='node').get_text()
-    author = re.findall(r'(?<=<a href="/member/).+(?="><img)', str(article))[0]
-    u = article.select('.item_title > a')
-    link = 'https://www.v2ex.com' + re.findall(r'(?<=href=").+(?=")', str(u))[0]
-    articles.append([title, category, author, link])
+for page in soup.find_all(class_='tab'):
+    tab = re.findall(r'(?<=href="/\?tab=).+(?=">)', str(page))
+    url = urlmode+tab[0]
+    html = requests.get(url).text
+    msoup = BeautifulSoup(html, 'html.parser')
+    for article in msoup.find_all(class_='cell item'):
+        title = article.find(class_='item_title').get_text().encode('utf-8').decode('utf-8', 'ignore')
+        category = article.find(class_='node').get_text()
+        author = re.findall(r'(?<=<a href="/member/).+(?="><img)', str(article))[0]
+        u = article.select('.item_title > a')
+        link = 'https://www.v2ex.com' + re.findall(r'(?<=href=").+(?=")', str(u))[0]
+        articles.append([tab[0], title, category, author, link])
 
-with open('v2ex.csv', 'w') as f:
+print(articles)
+with open('v2ex.csv', 'w', encoding='utf-8') as f:
     writer = csv.writer(f)
-    writer.writerow(['文章标题', '分类', '作者', '文章地址'])
+    writer.writerow(['大标题', '文章标题', '分类', '作者', '文章地址'])
     for row in articles:
         writer.writerow(row)
